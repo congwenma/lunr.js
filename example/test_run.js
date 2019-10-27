69132c3b885d7038268eb435047b7915e011b41d
Congwen Ma <congwen.ma@rallyhealth.com>
2019-10-26 22:26:40 -0700

    [annotated]

--- example/test_run.js
+++ example/test_run.js
@@ -1,19 +1,2 @@
-
-const lunr = require('../lunr.js');
-const inputPrograms = require('./input.json');
-
-debugger
-var idx = lunr(function() {
-  this.ref('program_id');
-  this.field('patient_first_name');
-  this.field('patient_last_name');
-
-  // removing stemmer
-  this.pipeline.reset();
-  debugger
-  inputPrograms.forEach(doc => this.add(doc))
-});
-
-debugger
-idx.search('maria')
-
+require('./test_run_from_github')
+// require('./test_run_recover_programs')
